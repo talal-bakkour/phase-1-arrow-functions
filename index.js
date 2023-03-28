dced5ba084cb5847289a103b44eb19c93daedd38
talal <ta440237@gmail.com>
2023-03-28 16:17:05 +0300

    compleated

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+  }
+  
+  const square = (num) => {
+    return num * num;
+  }
+  
+  
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
+
+  module.exports = { divide, square, add };
+  
+
